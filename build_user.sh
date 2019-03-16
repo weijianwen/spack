8b3ad0c482413c7419c34a7404518c5d6ec7ea1d
Jianwen WEI <weijianwen@gmail.com>
2019-03-17 01:19:50 +0800

    Fix intel@19.0.3 .

--- build_user.sh
+++ build_user.sh
@@ -17,5 +17,5 @@ for pkg in ["jdk@1.8.0_181-b13%gcc@4.8.5",
             "intel-parallel-studio@cluster.2017.5+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune %intel@17.0.5 threads=openmp",
             "intel-parallel-studio@cluster.2018.1+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune %intel@18.0.1 threads=openmp",
             "intel-parallel-studio@cluster.2018.3+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune %intel@18.0.3 threads=openmp",
-            "intel-parallel-studio@cluster.2019.2+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune %intel@19.0.2 threads=openmp"]:
+            "intel-parallel-studio@cluster.2019.3+advisor+clck+daal+inspector+ipp+itac+mkl+mpi+tbb+vtune %intel@19.0.3 threads=openmp"]:
            os.system("spack install {}".format(pkg))
